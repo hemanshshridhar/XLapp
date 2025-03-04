11eefd03cf1cd40ab2af4c2837c8192c2296914b
hemanshshridhar <121416974+hemanshshridhar@users.noreply.github.com>
2025-03-04 17:07:14 +0530

    Update llmapp.py

--- llmapp.py
+++ llmapp.py
@@ -12,10 +12,9 @@ from fastapi import FastAPI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
 
-# load_dotenv()
-# os.environ["LANGCHAIN_TRACING_V2"] = "true"
-# os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-# os.environ['OPENAI_API_KEY']=     os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+api_key = st.secrets["OPENAI_API_KEY"]
+langchain_key = st.secrets["LANGCHAIN_API_KEY"]
 model = ChatOpenAI(model_name="gpt-4")
 
 def clean_and_parse_json(response):
