adb72bbc4f39babb18fe88d37163b01ac46daccd
hemanshshridhar <121416974+hemanshshridhar@users.noreply.github.com>
2025-04-03 15:06:55 +0530

    Update llmapp.py

--- llmapp.py
+++ llmapp.py
@@ -17,7 +17,8 @@ from langchain.chat_models import ChatOpenAI
 # langchain_key = st.secrets["LANGCHAIN_API_KEY"]
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+# os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+langchain_key = st.secrets["LANGCHAIN_API_KEY"]
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 model = ChatOpenAI(model_name="gpt-4", openai_api_key=openai.api_key)
 
